6592ee6c7d9d959ce5187a387a9de8c9ea23a2e2
ruti-h <r0548428867@gmail.com>
2025-06-08 15:27:13 +0300

    fix-s3

---
+++ Api/Cheers/Cheers.Core/DTOs/UploadFileRequest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers.Core.DTOs
+{
+    public class UploadFileRequest
+    {
+        public IFormFile File { get; set; }
+
+    }
+}
